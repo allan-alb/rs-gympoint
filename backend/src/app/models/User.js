db5ff90b85c2eccbccaea0ae2088fdbc7a6ae5b0
allan09alb <allan.alb09@gmail.com>
2020-04-08 17:39:25 -0300

    1 - Estrutura básica e BD configurados

---
+++ backend/src/app/models/User.js
@@ -0,0 +1,19 @@
+import { Models, Sequelize } from 'sequelize';
+
+class User extends Models {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+export default User;
---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'allan',
+  password: 'allan123',
+  database: 'gympoint',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
